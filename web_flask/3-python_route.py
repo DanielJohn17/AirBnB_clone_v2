a8e2e0d1738a254fca4d1b1a43c888daf6bede5f
DanielJohn17 <danielyohannes38@gmail.com>
2023-12-30 18:19:31 +0300

    Python flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Script that runs an app with Flask framework """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function called with / route """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Function called with /hbnb route """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_txt(text='is cool'):
+    """ Function called with /hbnb route """
+    if text is not 'is cool':
+        return 'Python {}'.format(text.replace('_', ' '))
+    else:
+        return 'Python {}'.format(text)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
