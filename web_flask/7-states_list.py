b225e41fbba074200bcd61e6f950035ef9848bc9
DanielJohn17 <danielyohannes38@gmail.com>
2024-01-02 22:29:11 +0300

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Script that runs an app with Flask framework """
-import sys
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-print(sys.path)
+
 app = Flask(__name__)
 
 
